e143ba63e7cff96d9a284ee180464ac9e5e27124
bekzatsa <bekzat.saylaubay@gmail.com>
2026-03-01 01:35:38 +0500

    feat(01-01): add Maven dependencies and Kotlin JPA compiler plugins
    - Add spring-boot-starter-data-jpa, spring-security, oauth2-authorization-server, validation starters
    - Configure Kotlin compiler with jpa, all-open, spring plugins
    - Add all-open annotations for jakarta.persistence.Entity/MappedSuperclass/Embeddable
    - Add kotlin-maven-noarg plugin dependency for JPA no-arg constructors
    - Exclude OAuth2AuthorizationServerAutoConfiguration from TemplateApplication to prevent competing SecurityFilterChain

--- src/main/kotlin/kz/innlab/template/TemplateApplication.kt
+++ src/main/kotlin/kz/innlab/template/TemplateApplication.kt
@@ -3,9 +3,11 @@ package kz.innlab.template
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [
+    org.springframework.boot.security.oauth2.server.authorization.autoconfigure.servlet.OAuth2AuthorizationServerAutoConfiguration::class
+])
 class TemplateApplication
 
 fun main(args: Array<String>) {
-	runApplication<TemplateApplication>(*args)
+    runApplication<TemplateApplication>(*args)
 }
